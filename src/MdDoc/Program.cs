d699a78ca00248c2c644f846e3e11c05f460d5ed
Andreas Gruenwald <andreasgruenwald@live.de>
2018-11-04 17:29:41 +0100

    Remove hardcoded paths

--- src/MdDoc/Program.cs
+++ src/MdDoc/Program.cs
@@ -9,16 +9,15 @@ namespace MdDoc
 {
     class Program
     {
-        const string s_Path = @"C:\Users\Andreas\Desktop\grynwald.markdowngenerator.2.0.78\lib\netstandard2.0\Grynwald.MarkdownGenerator.dll";
-        const string s_OutDir = @"C:\Users\Andreas\Desktop\tmp";
-
         static void Main(string[] args)
         {
-            if (Directory.Exists(s_OutDir))
-                Directory.Delete(s_OutDir, true);
+            var assemblyPath = args[0];
+            var outDir = args[1];
 
+            if (Directory.Exists(outDir))
+                Directory.Delete(outDir, true);
 
-            var documentationWriter = new DocumentationWriter(s_Path, s_OutDir);
+            var documentationWriter = new DocumentationWriter(assemblyPath, outDir);
             documentationWriter.SaveDocumentation();
 
             
