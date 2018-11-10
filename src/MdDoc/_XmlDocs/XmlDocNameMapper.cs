3a9dd0c82fd04b82be649b8c9ddd210c297dbea8
Andreas Grünwald <andreasgruenwald@live.de>
2018-11-10 15:44:40 +0100

    Clean up XmlDocNameMapper

--- src/MdDoc/_XmlDocs/XmlDocNameMapper.cs
+++ src/MdDoc/_XmlDocs/XmlDocNameMapper.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using Mono.Cecil;
-using System.Text;
+﻿using System.Text;
 using System.Linq;
+using Mono.Cecil;
 
 namespace MdDoc
 {
     class XmlDocNameMapper
-    {
-
-        public string GetXmlDocName(TypeReference type)
-        {
-            return $"T:{type.FullName}";
-        }
+    {        
+        public string GetXmlDocName(TypeReference type) => $"T:{type.FullName}";
 
         public string GetXmlDocName(MethodDefinition method)
         {
             var stringBuilder = new StringBuilder();
 
+            // methods are prefixed with "M:", followed by the type name 
             stringBuilder.Append("M:");
-
             stringBuilder.Append(method.DeclaringType.FullName);
             stringBuilder.Append(".");
 
+            // the name of the method is "#ctor" if it is a constructor
             if (method.IsConstructor)
             {
                 stringBuilder.Append("#ctor");
@@ -31,36 +26,26 @@ namespace MdDoc
             {
                 stringBuilder.Append(method.Name);
             }
-
-
+        
+            //If the method has generic parameters, the number of parameters is appended to the name
             if(method.HasGenericParameters)
             {
                 stringBuilder.Append("``");
                 stringBuilder.Append(method.GenericParameters.Count);
             }
 
-
+            // Append parameters
             if (method.HasParameters)
             {
-                stringBuilder.Append("(");
+                var parameterNames = method.Parameters.Select(p => GetSerializedTypeName(p.ParameterType));
 
-                var first = true;
-                foreach (var parameter in method.Parameters)
-                {
-                    if (!first)
-                    {
-                        stringBuilder.Append(",");
-                    }                    
-                    
-                    stringBuilder.Append(GetSerializedTypeName(parameter.ParameterType));                    
-
-                    first = false;
-
-                }
+                stringBuilder.Append("(");            
+                stringBuilder.AppendJoin(",", parameterNames);               
                 stringBuilder.Append(")");
             }
 
-            
+            // if the method overloads the conversion operator, 
+            // the return type is appended to the name (separated by '~')
             if (method.IsSpecialName)
             {
                 if(method.Name == "op_Implicit" || method.Name == "op_Explicit")
@@ -77,27 +62,21 @@ namespace MdDoc
         {
             var stringBuilder = new StringBuilder();
 
+            // properties are prefixed with "P:", followed by the type name 
             stringBuilder.Append("P:");
-
             stringBuilder.Append(property.DeclaringType.FullName);
             stringBuilder.Append(".");
+
+            // append property name
             stringBuilder.Append(property.Name);
 
+            // indexers are implemented as properties with parameters
             if (property.HasParameters)
             {
+                var parameterNames = property.Parameters.Select(p => GetSerializedTypeName(p.ParameterType));
+
                 stringBuilder.Append("(");
-
-                var first = true;
-                foreach (var parameter in property.Parameters)
-                {
-                    if (!first)
-                    {
-                        stringBuilder.Append(",");
-                    }
-                    stringBuilder.Append(GetSerializedTypeName(parameter.ParameterType));
-                    first = false;
-
-                }
+                stringBuilder.AppendJoin(",", parameterNames);
                 stringBuilder.Append(")");
             }
 
@@ -105,21 +84,23 @@ namespace MdDoc
         }
 
         public string GetXmlDocName(FieldDefinition field)
-        {
-            var stringBuilder = new StringBuilder();
-
-            stringBuilder.Append("F:");
-
-            stringBuilder.Append(field.DeclaringType.FullName);
-            stringBuilder.Append(".");
-            stringBuilder.Append(field.Name);
-
-            return stringBuilder.ToString();
+        {            
+            // methods are prefixed with "F:", followed by the type and field names
+            return $"F:{field.DeclaringType.FullName}.{field.Name}";            
         }
 
-        private string GetSerializedTypeName(TypeReference type)
-        {
 
+        private string GetSerializedTypeName(TypeReference type)
+        {      
+            //
+            // Generic parameters are serialized as the index of the parameter, prefixed by ` or ``
+            // - '`' refers to a generic parameter of a type
+            // - '``' refers to a generic parameter of a method
+            //
+            // Examples:
+            //  - A type that is the first type parameter of a type is serialized as '`0'
+            //  - A type that is the second type parameter of a method is serialized as '``1'
+            //
             if (type.IsGenericParameter)
             {
                 var genericParameter = (GenericParameter)type;
@@ -135,41 +116,32 @@ namespace MdDoc
                 }
             }
 
-
+            //
+            // Generic arguments, e.g. IEnumerable<string> 
+            // are serialized as TYPENAME{ARGUMENT1,ARGUMENT2}
+            //
             if (type is GenericInstanceType genericType && genericType.HasGenericArguments)
             {
-                var arguments = genericType.GenericArguments;
-
-                var typeName = genericType.Name.Replace($"`{arguments.Count}", "");
+                // in XML docs, the number of generic arguments is not part of the type name.
+                // Thus, we need to remove the ` suffix
+                var typeName = genericType.Name.Replace($"`{genericType.GenericArguments.Count}", "");
 
                 var stringBuilder = new StringBuilder();
 
+                // append name of the type (namespace + name)
                 stringBuilder.Append(type.Namespace);
                 stringBuilder.Append(".");
                 stringBuilder.Append(typeName);
+
                 stringBuilder.Append("{");
-
-                var first = true;
-                foreach (var typeArgument in arguments)
-                {
-                    if (!first)
-                    {
-                        stringBuilder.Append(",");
-                    }
-                    stringBuilder.Append(GetSerializedTypeName(typeArgument));
-                    first = false;
-
-                }
-
+                stringBuilder.AppendJoin(",", genericType.GenericArguments.Select(GetSerializedTypeName));
                 stringBuilder.Append("}");
 
                 return stringBuilder.ToString();
-
             }
 
+            // if no generics are involved, simply return the type name
             return type.FullName;
-
-        }
-        
+        }        
     }
 }
