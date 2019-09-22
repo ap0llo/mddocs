b9b0618d3c05acc7076f70be48aca01b405565af
Andreas Gruenwald <andreas@grynwald.net>
2019-09-22 12:20:18 +0200

    WIP: MSBuild integration
    Add new project for custom MSBuild tasks

---
+++ src/MdDocs.MSBuild/src/GenerateCommandLineReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Grynwald.MdDocs.MSBuild
+{
+    public sealed class GenerateCommandLineReference : Task
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
