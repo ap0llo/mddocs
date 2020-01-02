35e1f7f3104c7f0adc4c0f350ce71dbbce38758c
Andreas Gruenwald <andreas@grynwald.net>
2020-01-02 21:33:54 +0100

    WIP: Dynamically create test data
    Reimplement AssemblyDocumentationTest using dynamically generated assembly.

---
+++ src/MdDocs.ApiReference.Model.Test/AssemblyDocumentationTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using Grynwald.MdDocs.ApiReference.Model.XmlDocs;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Grynwald.MdDocs.ApiReference.Model.Test
+{
+    public class AssemblyDocumentationTest : DynamicCompilationTestBase
+    {
+        [Fact]
+        public void TryGetDocumentation_returns_null_for_an_undocumented_type()
+        {
+            // ARRANGE
+            var cs = @"
+                namespace Namespace1.Namespace2
+                {
+                    internal class InternalClass1
+                    { }
+                }
+            ";
+
+            using var assembly = Compile(cs);
+
+            var typeId = assembly.MainModule.Types.Single(x => x.Name == "InternalClass1").ToTypeId();
+            var sut = new AssemblyDocumentation(assembly, NullXmlDocsProvider.Instance, NullLogger.Instance);
+
+            // ACT
+            var documentation = sut.TryGetDocumentation(typeId);
+
+            // ASSERT
+            Assert.Null(documentation);
+        }
+
+        [Fact]
+        public void TryGetDocumentation_returns_expected_documentation_item_for_an_documented_type()
+        {
+            // ARRANGE
+            var cs = @"
+                namespace Namespace1.Namespace2
+                {
+                    public class Class1
+                    { }
+                }
+            ";
+
+            using var assembly = Compile(cs);
+
+            var typeId = assembly.MainModule.Types.Single(x => x.Name == "Class1").ToTypeId();
+            var sut = new AssemblyDocumentation(assembly, NullXmlDocsProvider.Instance, NullLogger.Instance);
+
+            // ACT
+            var documentation = sut.TryGetDocumentation(typeId);
+
+            // ASSERT
+            Assert.NotNull(documentation);
+            var typeDocumentation = Assert.IsType<TypeDocumentation>(documentation);
+            Assert.Equal(typeId, typeDocumentation.TypeId);
+        }
+    }
+}
