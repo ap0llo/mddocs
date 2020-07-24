bd34032cfa67909bd33a450e858be7a1450baee9
Andreas Gruenwald <andreas@grynwald.net>
2020-07-24 14:45:56 +0200

    chore: Use nbgv's json output foramt in New-Release script.
    Use nbgv's option to output json to get the name of the new branch instead of parsing the log output.

--- utilities/New-Release.ps1
+++ utilities/New-Release.ps1
@@ -1,5 +1,5 @@
 #
-# This script creates a new release branch for the current version, increments 
+# This script creates a new release branch for the current version, increments
 # the version on master and pushes both master and the new release branch.
 #
 
@@ -34,14 +34,14 @@ function copySchema {
         $versionSpecificSchemaDirectory = "./schemas/configuration/v$majorMinor/"
         New-Item -ItemType Directory -Path $versionSpecificSchemaDirectory  | Out-Null
         Copy-Item -Path $latestSchemaPath  -Destination $versionSpecificSchemaDirectory
-                
+
         # Commit changes
         exec "git add `"$versionSpecificSchemaDirectory`""
         exec "git commit -m `"Add v$majorMinor configuration schema`" "
     }
     finally {
         Pop-Location
-    }    
+    }
 }
 
 
@@ -61,20 +61,8 @@ try {
     exec "dotnet tool restore"
 
     log "Creating release branch"
-    $nbgvOutput = exec "dotnet tool run nbgv -- prepare-release"
-    Write-Host $nbgvOutput
-
-    # Find the name of the release branch
-    $outputLines = $nbgvOutput.Split([System.Environment]::NewLine)
-    $releaseBranchName = $null
-    foreach ($line in $outputLines) {
-        # The line logging the release branch name looks something like this:
-        # 'release/v0.2 branch now tracks v0.2 stabilization and release.'
-        if ($line.Contains("stabilization and release") -and $line.Contains("branch")) {
-            
-            $releaseBranchName = $line.Substring(0, $line.IndexOf("branch")).Trim()
-        }
-    }
+    $nbgvOutput = exec "dotnet tool run nbgv -- prepare-release --format json" | ConvertFrom-Json
+    $releaseBranchName = $nbgvOutput.NewBranch.Name
 
     if ($releaseBranchName) {
         log "Release branch name is '$releaseBranchName'"
