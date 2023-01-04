44cecb034bb62bf9aef332156e464cb46a7098f3
Andreas Grünwald <andreas@grynwald.net>
2023-01-04 16:48:01 +0100

    Rewrite MSBuild "GenerateCommandLineDocumentation" target to start the MdDocs CLI instead of using the GenerateCommandLineDocumentation MSBuild task
    Run generation of commandline helpas child-process instead of loading MdDocs into the MSBuild process

--- src/MdDocs.MSBuild.IntegrationTest/MSBuildIntegrationTest.OutOfProcess.cs
+++ src/MdDocs.MSBuild.IntegrationTest/MSBuildIntegrationTest.OutOfProcess.cs
@@ -10,7 +10,7 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
     {
         [Theory]
         [MemberData(nameof(MSBuildRuntimesData))]
-        public void Apireference_command_is_started_during_build(MSBuildRuntimeInfo msbuildRuntime)
+        public void ApiReference_command_is_started_during_build(MSBuildRuntimeInfo msbuildRuntime)
         {
             // ARRANGE
             using var project = new MSBuildTestProject(m_OutputHelper);
@@ -34,8 +34,8 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
                 .InstallNuGetPackage(package)
                 .ConfigureMdDocs(x =>
                 {
-                    x.ApiReferenceDocumentationOutputPath = configuredApiReferenceOutputDirectory;
                     x.GenerateApiReferenceDocumentationOnBuild = true;
+                    x.ApiReferenceDocumentationOutputPath = configuredApiReferenceOutputDirectory;
                 });
 
             // ACT
@@ -51,7 +51,7 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
         [InlineData(new string[] { "mddocs.settings.json" }, null, "mddocs.settings.json")]
         [InlineData(new string[] { "mddocs.settings.json", "config/mddocs/settings.json" }, null, "mddocs.settings.json")]
         [InlineData(new string[] { "mddocs.settings.json", "config/mddocs/settings.json" }, "config/mddocs/settings.json", "config/mddocs/settings.json")]
-        public void Apireference_command_is_started_with_the_expected_configuration_file(string[] configurationFilesOnDisk, string? configuredConfigurationFile, string expectedConfigurationFile)
+        public void ApiReference_command_is_started_with_the_expected_configuration_file_parameter(string[] configurationFilesOnDisk, string? configuredConfigurationFile, string expectedConfigurationFile)
         {
             // ARRANGE
             using var project = new MSBuildTestProject(m_OutputHelper);
@@ -94,7 +94,7 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
         }
 
         [Fact]
-        public void Expected_documentation_is_generated_during_build()
+        public void ApiReference_Expected_files_are_generated_during_build()
         {
             // ARRANGE
             using var project = new MSBuildTestProject(m_OutputHelper);
@@ -119,8 +119,8 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
                     """)
                 .ConfigureMdDocs(x =>
                 {
-                    x.ApiReferenceDocumentationOutputPath = configuredApiReferenceOutputDirectory;
                     x.GenerateApiReferenceDocumentationOnBuild = true;
+                    x.ApiReferenceDocumentationOutputPath = configuredApiReferenceOutputDirectory;
                 });
 
             // ACT
@@ -144,16 +144,12 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
         }
 
         [Fact]
-        public void Warnings_written_to_the_console_are_emitted_as_MSBuild_warnings()
+        public void ApiReference_Warnings_written_to_the_console_are_emitted_as_MSBuild_warnings()
         {
             // ARRANGE
             using var project = new MSBuildTestProject(m_OutputHelper);
 
             var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
-
-            // Paths
-            var configuredApiReferenceOutputDirectory = "api/";
-            var absoluteApiReferenceOutputDirectory = project.ProjectDirectory.PathCombine(configuredApiReferenceOutputDirectory).GetFullPath().TrimEndingDirectorySeparator();
 
             // Set up project
             project
@@ -161,7 +157,7 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
                 .ConfigureMdDocs(x =>
                 {
                     x.GenerateApiReferenceDocumentationOnBuild = true;
-                    x.ApiReferenceDocumentationOutputPath = configuredApiReferenceOutputDirectory;
+                    x.ApiReferenceDocumentationOutputPath = "api/";
                 });
 
             // ACT
@@ -177,7 +173,7 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
         }
 
         [Fact]
-        public void Errors_written_to_the_console_are_emitted_as_MSBuild_errors()
+        public void ApiReference_Errors_written_to_the_console_are_emitted_as_MSBuild_errors()
         {
             // ARRANGE
             using var project = new MSBuildTestProject(m_OutputHelper);
@@ -187,16 +183,200 @@ namespace Grynwald.MdDocs.MSBuild.IntegrationTest
             // Set up project
             project
                 .InstallNuGetPackage(package)
-                .ConfigureMdDocs(x =>
-                {
-                    x.GenerateApiReferenceDocumentationOnBuild = true;
-                });
+                .ConfigureMdDocs(x => x.GenerateApiReferenceDocumentationOnBuild = true);
 
             // ACT
             var result = project.Build(MSBuildRuntimes.Default);
 
             // ASSERT
             var target = Assert.Single(result.BinaryLog.GetTargets("GenerateApiReferenceDocumentation"));
+            var execTask = Assert.Single(target.GetTasks("Exec"));
+
+            var errors = execTask.FindChildrenRecursive<Error>();
+            Assert.Contains(errors, err => err.Text.Contains("ERROR - Invalid output directory"));
+        }
+
+        [Theory]
+        [MemberData(nameof(MSBuildRuntimesData))]
+        public void CommandLineHelp_command_is_started_during_build(MSBuildRuntimeInfo msbuildRuntime)
+        {
+            // ARRANGE
+            using var project = new MSBuildTestProject(m_OutputHelper);
+
+            var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
+            var mddocsCliAssemblyPath = project.ResolveFileFromPackage(package.Identity, "tools/net6.0/mddocs.dll");
+
+            // Paths
+            var configuredOutputDirectory = "commandlinehelp/";
+            var absoluteOutputDirectory = project.ProjectDirectory.PathCombine(configuredOutputDirectory).GetFullPath().TrimEndingDirectorySeparator();
+
+            // Expected arguments
+            var expectedArguments = new ProcessArgumentsBuilder()
+                .Append("dotnet").AppendQuoted(mddocsCliAssemblyPath)
+                .Append("commandlinehelp")
+                .Append("--assembly").AppendQuoted(project.TargetPath)
+                .Append("--outdir").AppendQuoted(absoluteOutputDirectory);
+
+            // Set up project
+            project
+                .InstallNuGetPackage(package)
+                .ConfigureMdDocs(x =>
+                {
+                    x.GenerateCommandLineDocumentationOnBuild = true;
+                    x.CommandLineDocumentationOutputPath = configuredOutputDirectory;
+                });
+
+            // ACT
+            var result = project.Build(msbuildRuntime);
+
+            // ASSERT
+            Assert.Equal(0, result.ExitCode);
+            AssertCommandLineArguments(expectedArguments, result, "GenerateCommandLineDocumentation");
+        }
+
+        [Theory]
+        [InlineData(new string[0], null, null)]
+        [InlineData(new string[] { "mddocs.settings.json" }, null, "mddocs.settings.json")]
+        [InlineData(new string[] { "mddocs.settings.json", "config/mddocs/settings.json" }, null, "mddocs.settings.json")]
+        [InlineData(new string[] { "mddocs.settings.json", "config/mddocs/settings.json" }, "config/mddocs/settings.json", "config/mddocs/settings.json")]
+        public void CommandLineHelp_command_is_started_with_the_expected_configuration_file_parameter(string[] configurationFilesOnDisk, string? configuredConfigurationFile, string expectedConfigurationFile)
+        {
+            // ARRANGE
+            using var project = new MSBuildTestProject(m_OutputHelper);
+
+            var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
+            var mddocsCliAssemblyPath = project.ResolveFileFromPackage(package.Identity, "tools/net6.0/mddocs.dll");
+
+            // Expected arguments
+            var expectedArguments = new ProcessArgumentsBuilder()
+                .Append("dotnet").AppendQuoted(mddocsCliAssemblyPath)
+                .Append("commandlinehelp")
+                .Append("--assembly").AppendQuoted(project.TargetPath);
+
+            if (expectedConfigurationFile is not null)
+            {
+                expectedArguments
+                    .Append("--configurationFilePath")
+                    .AppendQuoted(project.ProjectDirectory.PathCombine(expectedConfigurationFile).GetFullPath());
+            }
+
+            // Set up project
+            project
+                .InstallNuGetPackage(package)
+                .ConfigureMdDocs(x =>
+                {
+                    x.GenerateCommandLineDocumentationOnBuild = true;
+                    x.ConfigurationFilePath = configuredConfigurationFile;
+                });
+
+            foreach (var configurationFile in configurationFilesOnDisk)
+            {
+                project.AddFile(configurationFile, "{ }");
+            }
+
+            // ACT
+            var result = project.Build(MSBuildRuntimes.Default);
+
+            // ASSERT
+            AssertCommandLineArguments(expectedArguments, result, "GenerateCommandLineDocumentation");
+        }
+
+        [Fact]
+        public void CommandLineHelp_Expected_files_are_generated_during_build()
+        {
+            // ARRANGE
+            using var project = new MSBuildTestProject(m_OutputHelper);
+
+            var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
+
+            // Paths
+            var configuredOutputDirectory = "commandlinehelp/";
+            var absoluteOutputDirectory = project.ProjectDirectory.PathCombine(configuredOutputDirectory).GetFullPath().TrimEndingDirectorySeparator();
+
+            // Set up project
+            project
+                .InstallNuGetPackage(package)
+                .AddFile("Class1.cs",
+                    """
+                    namespace MyNamespace
+                    {
+                        public class Class1
+                        {
+                        }
+                    }
+                    """)
+                .ConfigureMdDocs(x =>
+                {
+                    x.GenerateCommandLineDocumentationOnBuild = true;
+                    x.CommandLineDocumentationOutputPath = configuredOutputDirectory;
+                });
+
+            // ACT
+            var result = project.Build(MSBuildRuntimes.Default);
+
+            // ASSERT
+            Assert.Equal(0, result.ExitCode);
+
+            var files = Directory
+                    .GetFiles(absoluteOutputDirectory, "*", SearchOption.AllDirectories)
+                    .Select(x => Path.GetRelativePath(absoluteOutputDirectory, x))
+                    .Select(x => x.Replace("\\", "/"))
+                    .Order(StringComparer.Ordinal);
+
+            Assert.Collection(
+                files,
+                x => Assert.Equal("index.md", x)
+            );
+        }
+
+        [Fact]
+        public void CommandLineHelp_Warnings_written_to_the_console_are_emitted_as_MSBuild_warnings()
+        {
+            // ARRANGE
+            using var project = new MSBuildTestProject(m_OutputHelper);
+
+            var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
+
+
+            // Set up project
+            project
+                .InstallNuGetPackage(package)
+                .ConfigureMdDocs(x =>
+                {
+                    x.GenerateCommandLineDocumentationOnBuild = true;
+                    x.CommandLineDocumentationOutputPath = "commandlinehelp/";
+                });
+
+            // ACT
+            var result = project.Build(MSBuildRuntimes.Default);
+
+            // ASSERT
+            var target = Assert.Single(result.BinaryLog.GetTargets("GenerateCommandLineDocumentation"));
+            var execTask = Assert.Single(target.GetTasks("Exec"));
+
+            var warnings = execTask.FindChildrenRecursive<Warning>();
+            Assert.Contains(warnings, warning => warning.Text.Contains("WARNING - No option classes found"));
+
+        }
+
+        [Fact]
+        public void CommandLineHelp_Errors_written_to_the_console_are_emitted_as_MSBuild_errors()
+        {
+            // ARRANGE
+            using var project = new MSBuildTestProject(m_OutputHelper);
+
+            var package = m_PackagesFixture.GetPackage("Grynwald.MdDocs.MSBuild");
+
+            // Set up project
+            project
+                .InstallNuGetPackage(package)
+                .ConfigureMdDocs(x => x.GenerateCommandLineDocumentationOnBuild = true);
+
+            // ACT
+            var result = project.Build(MSBuildRuntimes.Default);
+
+            // ASSERT
+            var target = Assert.Single(result.BinaryLog.GetTargets("GenerateCommandLineDocumentation"));
             var execTask = Assert.Single(target.GetTasks("Exec"));
 
             var errors = execTask.FindChildrenRecursive<Error>();
