ace65d25a19adbc66579effb70f1561018f9007a
Andreas Grünwald <andreas@grynwald.net>
2023-01-04 17:26:37 +0100

    Remove redundant MSBuild tasks "GenerateApiReferenceDocumentation" and "GenerateCommandLineDocumentation"
    Tasks are no longer necessary since MSBuild integration now runs the documentation generator as a child process.

--- src/MdDocs.MSBuild/src/_Extensions/TaskItemExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Build.Framework;
-
-namespace Grynwald.MdDocs.MSBuild
-{
-    internal static class TaskItemExtensions
-    {
-        public static string GetFullPath(this ITaskItem taskItem) => taskItem.GetMetadata("FullPath");
-    }
-}
